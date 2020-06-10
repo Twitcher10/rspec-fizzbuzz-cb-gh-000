35c0e93bdf6d031e7c10903126aa083067a4e877
Trevor Cheche <twitcherfrost@gmail.com>
2020-06-10 17:35:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,5 +7,7 @@ def fizzbuzz(int)
       "Buzz"
     end
   end
+
+fizzbuzz(int)
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
